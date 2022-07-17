dbb50e2106f74bd41db2d43d2a4cd9dc931f145c
Leondro Lio <github@leondrolio.com>
2022-07-17 03:49:51 +0100

    Button accessability features & disabled

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,11 +3,26 @@ import React, { MouseEventHandler, ReactNode } from "react";
 const Button = ({
   children,
   onClick,
+  disabled,
+  title = "",
 }: {
   children: ReactNode;
   onClick?: MouseEventHandler<HTMLDivElement>;
+  disabled?: boolean;
+  title?: string;
 }) => (
-  <div className="bg-blue-300 px-6 py-3" onClick={onClick}>
+  <div
+    className={`px-6 py-3 ${
+      disabled
+        ? "cursor-not-allowed bg-blue-100 text-gray-400"
+        : "cursor-pointer bg-blue-300"
+    }`}
+    onClick={onClick}
+    title={title}
+    role="button"
+    tabIndex={0}
+    aria-disabled={disabled}
+  >
     {children}
   </div>
 );
