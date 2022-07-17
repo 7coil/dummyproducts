43ec64e1aff2739b60ad80f8e894930c5a18da16
Leondro Lio <github@leondrolio.com>
2022-07-17 02:30:21 +0100

    Spruce up the 404 page

--- src/pages/NotFoundPage/index.tsx
+++ src/pages/NotFoundPage/index.tsx
@@ -1,9 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { Layout } from "../../components/Layout";
+import { TextSection } from "../../components/TextSection";
 
 const NotFoundPage = () => (
   <Layout>
-    <h1>Not Found</h1>
+    <TextSection>
+      <h1>Not Found</h1>
+      <p>
+        Unfortunately, the content you were looking for does not exist, either
+        because it has been moved or because you have accessed a bad link.
+      </p>
+      <p>
+        <Link to="/">Go Home</Link>
+      </p>
+    </TextSection>
   </Layout>
 );
 
