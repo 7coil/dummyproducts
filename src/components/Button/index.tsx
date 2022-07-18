13e75f0e6f749405e72008006dd0c1f61fbece20
Leondro Lio <github@leondrolio.com>
2022-07-18 13:31:54 +0100

    Allow button colour customisation

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -10,18 +10,24 @@ const Button = ({
   disabled,
   title = "",
   type,
+  disabledColour = "bg-blue-100",
+  activeColour = "bg-blue-300",
+  hoverColour = "hover:bg-blue-200",
 }: {
   children: ReactNode;
   onClick?: MouseEventHandler<HTMLButtonElement>;
   disabled?: boolean;
   title?: string;
   type?: ButtonHTMLAttributes<HTMLButtonElement>["type"];
+  disabledColour?: string;
+  activeColour?: string;
+  hoverColour?: string;
 }) => (
   <button
     className={`rounded px-6 py-3 ${
       disabled
-        ? "cursor-not-allowed bg-blue-100 text-gray-400"
-        : "cursor-pointer bg-blue-300 transition-colors hover:bg-blue-200"
+        ? `cursor-not-allowed ${disabledColour} text-gray-400`
+        : `cursor-pointer ${activeColour} transition-colors ${hoverColour}`
     }`}
     onClick={onClick}
     title={title}
